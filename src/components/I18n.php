b4798c9e529e8ce96eb20190f3b36040f94c4d5d
Ivan Pushkin <imetalguardi@gmail.com>
2015-06-21 07:41:56 +0000

    testing message source

--- src/components/I18n.php
+++ src/components/I18n.php
@@ -2,7 +2,7 @@
 
 namespace metalguardian\i18n\components;
 
-use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
 use yii\i18n\DbMessageSource;
 
 /**
@@ -10,43 +10,19 @@ use yii\i18n\DbMessageSource;
  */
 class I18n extends \yii\i18n\I18N
 {
-    /** @var array */
-    public $languages;
-    /** @var array */
-    public $missingTranslationHandler = ['metalguardian\i18n\TranslationEventHandler', 'handleMissingTranslation'];
-    public $messageSource = [
-        'class' => DbMessageSource::class,
-    ];
+    public $messageSourceConfig = [];
 
     public function init()
     {
         parent::init();
 
-        if ($this->languages instanceof \Closure) {
-            $this->languages = call_user_func($this->languages);
-        }
+        $defaults = [
+            'class' => DbMessageSource::className(),
+            'on missingTranslation' => ['metalguardian\i18n\TranslationEventHandler', 'handleMissingTranslation'],
+        ];
+        $config = ArrayHelper::merge($defaults, $this->messageSourceConfig);
 
-        if (!is_array($this->languages)) {
-            throw new InvalidConfigException('I18n::languages have to be array.');
-        }
-
-        if (empty($this->languages)) {
-            throw new InvalidConfigException('I18n::languages have to contains at least 1 item.');
-        }
-
-        if (!isset($this->translations['*'])) {
-            $this->translations['*'] = [
-                'class' => DbMessageSource::className(),
-                'on missingTranslation' => $this->missingTranslationHandler
-            ];
-        }
-        if (!isset($this->translations['app']) && !isset($this->translations['app*'])) {
-            $this->translations['app'] = [
-                'class' => DbMessageSource::className(),
-                'on missingTranslation' => $this->missingTranslationHandler
-            ];
-        }
-
+        $this->translations['*'] = $config;
     }
 
 }
